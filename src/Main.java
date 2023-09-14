3dcb5cafdf36c2fbad2c0e0f54288dace594b55a
chubbyhubbybubby <austinc2026@cis.edu.hk>
2023-09-14 17:04:37 +0800

    Lets go

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String [] args){
-       int age;
-       String firstname;
+       int minimum_age = 18;
+       minimum_age = 7 + (minimum_age / 2);
+       System.out.println(minimum_age);
+       int age = 16;
+       int pageToRead=100-age;
+       System.out.println(pageToRead);
+    }
+
 }
 
 
